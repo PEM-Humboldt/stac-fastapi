9895d9eb7e84da224b7c0747208c4a5c344a44c8
Johan Bext <arquitecturati@humboldt.org.co>
2026-07-16 15:19:29 -0500

    Modified entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,9 +3,14 @@ set -e
 
 if [ "${RUN_MIGRATIONS}" = "true" ]; then
     echo "Running PGStac migrations..."
-    echo pypgstac migrate --dsn "postgresql://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST_WRITER}:${POSTGRES_PORT}/${POSTGRES_DBNAME}"
     pypgstac migrate --dsn "postgresql://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST_WRITER}:${POSTGRES_PORT}/${POSTGRES_DBNAME}"
     echo "Migrations complete."
 fi
 
-exec python -m stac_fastapi.pgstac.app
+exec uvicorn stac_fastapi.pgstac.app:app \
+    --host "${APP_HOST:-0.0.0.0}" \
+    --port "${APP_PORT:-8082}" \
+    --workers "${WEB_CONCURRENCY:-1}" \
+    --root-path "${ROOT_PATH:-}" \
+    --proxy-headers \
+    --forwarded-allow-ips="${FORWARDED_ALLOW_IPS:-*}"
